231ffd14ac8049c10eb62ae8f97a964974ef2c21
Ethan Walker <ethanwalker23081@gmail.com>
2023-02-09 14:26:26 -0500

    updated test files with skeleton of tests

---
+++ test/publisher.test.js
@@ -0,0 +1,12 @@
+import React, { useState} from "react";
+import ReactDOM from "react-dom/client";
+import { /** sampleComponent */} from "../ReactComponents/sampleComponent" /** replace sampleComponent to whatever the component is called */
+
+describe("component test", () => {
+    beforeEach(() =>{
+
+    });
+    it("describe test here", () => {
+
+    });
+});
---
+++ test/user.test.js
@@ -0,0 +1,12 @@
+import React, { useState} from "react";
+import ReactDOM from "react-dom/client";
+import { sliderComponent } from "../ReactComponents/sliderComponent"
+
+describe("slider", () => {
+    beforeEach(() =>{
+
+    });
+    it("describe test here", () => {
+
+    });
+});
