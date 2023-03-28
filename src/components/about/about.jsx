28501682e3b03a81671f0705c6648e5332611386
Shannon Schmitt <83383502+SFUCicada@users.noreply.github.com>
2023-03-28 14:18:05 -0400

    about us page

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import "./about.css"
 const about = () => {
   return (
-    <div>about</div>
+    <div className='aboutPage'>
+      <h1>about our diocese</h1>
+      <div className='info'>
+        <h2>mass times</h2>
+        <p>monday-friday: 9am</p>
+        <p>saturday: 4pm</p>
+        <p>sunday: 8am | 10:30am</p>
+        <h2>confession times</h2>
+        <p>saturday: 3:30-3:45pm</p>
+        <p>sunday: 7:30am-7:45am | 10-10:15am</p>
+        <h2>phone number</h2>
+        <p>814-495-4873</p>
+        <h2>location</h2>
+        <p>751 locust street</p>
+        <p>st. michael, pa 15951-0103</p>
+        <p></p>
+      </div>
+    </div>
   )
 }
 
